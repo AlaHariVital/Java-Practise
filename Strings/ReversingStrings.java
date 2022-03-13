fa2a96eb2c0d7de585dfe960498447600a9a732d
AlaHariVital <100507619+AlaHariVital@users.noreply.github.com>
2022-03-13 23:23:55 +0530

    Update ReversingStrings.java

--- Strings/ReversingStrings.java
+++ Strings/ReversingStrings.java
@@ -1,3 +1,13 @@
+/*  
+    Write a program to reverse a string using StringBuffer. Reverse entire sentence in string and also reverse each word in the string. 
+    Prompt the user input from the terminal.
+    
+    Sample input: Learning made easy at bitLabs
+    
+    Expected output:
+    reversing entire string: sbaLtib ta ysae edam gninraeL
+    reversing each word at its place: gninraeL edam ysae ta sbaLtib  
+*/
 package Strings;
 
 import java.util.Scanner;
